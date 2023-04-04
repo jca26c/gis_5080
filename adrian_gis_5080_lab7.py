5e4d3a5d51dd58592ce920cb31036b96f76bbb17
jca26c <78522594+jca26c@users.noreply.github.com>
2023-04-04 00:24:39 -0500

    Update adrian_gis_5080_lab7.py

--- adrian_gis_5080_lab7.py
+++ adrian_gis_5080_lab7.py
@@ -41,3 +41,20 @@ band_combo = st.selectbox(
     ('Red/Green/Blue', 'NIR/Red/Green',  'SWIR2/SWIR1/NIR', 'NIR/SWIR1/Red','SWIR2/NIR/Red', 
              'SWIR2/SWIR1/Red', 'SWIR1/NIR/Blue', 'NIR/SWIR1/Blue', 'SWIR2/NIR/Green', 'SWIR1/NIR/Red'))
 year_widget = st.slider("Selected year:", min_value=1984, max_value=2020, value=2010)
+mask_radio = st.radio("Apply fmask(remove cloud, shadow, snow)",(""))
+index = st.selectbox(
+    'Normalized Difference Indes:',
+    ('Vegetation Index (NDVI)', 
+              'Water Index (NDWI)',
+              'Modified Water Index (MNDWI)',
+              'Snow Index (NDSI)',
+              'Soil Index (NDSI)',
+              'Burn Ratio (NBR)',
+              'Customized'))
+first_band = st.selectbox(
+    '1st band',
+    ('Blue', 'Green','Red','NIR', 'SWIR1', 'SWIR2'))
+second_band = st.selectbox(
+    '2nd band',
+    ('Blue', 'Green','Red','NIR', 'SWIR1', 'SWIR2'))
+threshold = st.slider("Threshold:", min_value=-1, max_value=1, value=0,step=0.01)
